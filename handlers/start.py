46d561478640e39f2342507adf539942a5e0faec
bhoomi-s-007 <71869549+bhoomi-s-007@users.noreply.github.com>
2021-03-17 18:00:06 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,7 +9,7 @@ async def start(_, message: Message):
     await message.reply_text(
         f"""<b>👋🏻 Hi {message.from_user.first_name}!</b>
 
-I am SU Music Player, an open-source bot that lets you play music in your Telegram groups.
+I am @VCPlay_Robot an open-source bot that lets you play music in your Telegram groups.
 
 Use the buttons below to know more about me.""",
         reply_markup=InlineKeyboardMarkup(
