e387c561fa84657c46277baa0c8258294f4e2a28
bhoomi-s-007 <71869549+bhoomi-s-007@users.noreply.github.com>
2021-03-30 12:51:14 +0530

    Update search.py

--- handlers/search.py
+++ handlers/search.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from helpers.filters import command, other_filters, other_filters2
 
 
-@Client.on_message(command(["search", "search@VCPlay_Robot"]) & other_filters)
+@Client.on_message(command(["search", "search@Mahi_bhai_7_bot"]) & other_filters)
 async def search(_, message: Message):
     await message.reply_text(
         "💁🏻‍♂️ Do you want to search for a YouTube video?",
